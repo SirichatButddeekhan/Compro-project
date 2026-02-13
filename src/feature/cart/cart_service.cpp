cd68d59973116c0b91468057d5039a84501a730b
Noppanat <pangpondzaza.1604@gmail.com>
2026-02-13 13:11:36 +0700

    addToCart

--- src/feature/cart/cart_service.cpp
+++ src/feature/cart/cart_service.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
 
+struct MenuItem{
+    int id;
+    string name;
+    double price;
+    bool available;
+    string descripstion;
+};
 
+struct CartItem{
+    MenuItem item;
+    int quantity;
+};
+
+void addToCart(vector<CartItem> &cart, MenuItem menu){
+    if(!menu.available){
+        cout << "Sold out";
+        return;
+    }
+
+    for(int i = 0; i < cart.size(); i++){
+        if(cart[i].item.id == menu.id){
+            cart[i].quantity++;
+            cout << "Menu +1";
+            return;
+        }
+    }
+
+    cart.push_back({menu,1});
+}
 
 int main(){
-    cout << "1000 bath";
+    
 
     return 0;
 }
