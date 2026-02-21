fddf34759edec9e7af5c260f57bc6d5614887c74
Noppanat <pangpondzaza.1604@gmail.com>
2026-02-21 21:01:38 +0700

    condition for show receipt

--- src/feature/cart/cart_service.cpp
+++ src/feature/cart/cart_service.cpp
@@ -11,14 +11,30 @@ int main(){
 
     item.loadMenu("MenuItem.txt");
     item.ShowMenuForTest(); //แสดงเมนูสำหรับทดสอบระบบเฉยๆ
-    int choice;
+    int choice; //ตัวแปรไว้รับค่าที่ผู้ใช้จะเลือก
     while(true){
         cout << "Choose Menu (Select 0 for end program)";
         cin >> choice;
         if(choice == 0) break;
-        item.addToCart(choice);
+        item.addToCart(choice); //ใช้ฟังก์ชันเพิ่มลงตะกร้า
     }
-    item.ShowReceipt();
+    char ans;
+    cout << "Do you want receipt (enter Y for Yes and N for Not)\n";
+    cin >> ans;
+    while(true){
+        if(ans == 'Y'){
+            item.ShowReceipt(); //แสดงใบจองสินค้า
+            break;
+        }
+        else if(ans == 'N'){
+            cout << "Good bye\n";
+            break;
+        }
+        else{
+            cout << "Invalid input\n";
+            cin >> ans;
+        }
+    }
 
     return 0;
 }
