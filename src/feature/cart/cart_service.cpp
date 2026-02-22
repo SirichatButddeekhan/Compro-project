dbb78fbdb5440c16a27c86faaef9310f06e2658b
Noppanat <pangpondzaza.1604@gmail.com>
2026-02-22 19:22:11 +0700

    Fix main + Show menu for test +Prototype removefromcartfunc + edit txt

--- src/feature/cart/cart_service.cpp
+++ src/feature/cart/cart_service.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <iomanip>
 #include "cart_service.h"
 using namespace std;
 
@@ -9,30 +10,79 @@ int main(){
     
     CartSystem item;
 
+    char mode = ' '; //ตัวแปรไว้เก็บเป็นเงื่อนไขโหมดที่ผู้ใช้เลือก
+    char choice; //ตัวแปรไว้รับค่าที่ผู้ใช้จะเลือก
+    int id; //ตัวแปรเลขสินค้า
+
     item.loadMenu("MenuItem.txt");
     item.ShowMenuForTest(); //แสดงเมนูสำหรับทดสอบระบบเฉยๆ
-    int choice; //ตัวแปรไว้รับค่าที่ผู้ใช้จะเลือก
+    
+
     while(true){
-        cout << "Choose Menu (Select 0 for end program)";
-        cin >> choice;
-        if(choice == 0) break;
-        item.addToCart(choice); //ใช้ฟังก์ชันเพิ่มลงตะกร้า
-    }
-    char ans;
-    cout << "Do you want receipt (enter Y for Yes and N for Not)\n";
-    cin >> ans;
-    while(true){
-        if(ans == 'Y'){
-            item.ShowReceipt(); //แสดงใบจองสินค้า
-            break;
+        //เลือกโหมด หรือถูกย้อนกลับมา 'B'
+        if(mode == ' '){
+            cout << "------------------------------ MODE ------------------------------\n";
+            cout << "[A] Add to cart [D] Remove form cart [C] Open cart [E] Exit\n";
+            cout << "Select mode : ";
+            cin >> choice;
+            choice = toupper(choice);
+
+            if(choice == 'E'){
+                cout << "Good bye";
+                break;
+            }
+            if(choice == 'A' || choice == 'D' || choice == 'C'){
+                mode = choice;
+            }
+            else{
+                cout << "Invalid input!!\n";
+                continue;
+            }
         }
-        else if(ans == 'N'){
-            cout << "Good bye\n";
-            break;
+        //mode add to cart 'A'
+        else if(mode == 'A'){
+            cout << "--------------- ADD TO CART MODE ---------------\n";
+            cout << "Enter menu id ([B] to back) : ";
+            cin >> choice;
+            if(choice == 'B'){
+                mode = ' ';
+                continue;
+            }
+            cin.putback(choice);
+            cin >> id;
+
+            item.addToCart(id);
         }
-        else{
-            cout << "Invalid input\n";
-            cin >> ans;
+        //mode remove to cart 'D'
+        else if(mode == 'D'){
+            cout << "------------ REMOVE FROM CART MODE ------------\n";
+            cout << "Enter menu id ([B] to back) : ";
+            cin >> choice;
+            if(choice == 'B'){
+                mode = ' ';
+                continue;
+            }
+            cin.putback(choice);
+            cin >> id;
+
+            item.removeFromCart(id);
+        }
+        //mode open cart 'C'
+        else if(mode == 'C'){
+            cout << "----------------- CART MODE -----------------\n";
+            item.ShowCart();
+            cout << "Select [R] Show Receipt [B] back : ";
+            cin >> choice;
+            choice = toupper(choice);
+            if(choice == 'R'){
+                item.ShowReceipt();
+                break;
+            }
+            else if(choice == 'B'){
+                mode = ' ';
+                continue;
+            }
+            else cout << "Invalid input!!\n";
         }
     }
 
--- src/feature/cart/cart_service.h
+++ src/feature/cart/cart_service.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
 //Data base Menu
@@ -29,19 +31,66 @@ class CartSystem{
     public:
         void loadMenu(string);
         void ShowMenuForTest(); //แสดงเมนูสำหรับทดสอบโค้ดเฉยๆ
-        void addToCart(int id);
-        double calcutale();
+        void addToCart(int);
+        void removeFromCart(int);
+        double calculate();
+        void ShowCart();
         void ShowReceipt();
 };
 
 //ฟังก์ชันโหลดเมนูจากไฟล์
-void CartSystem::loadMenu(string){
+void CartSystem::loadMenu(string file){
+    ifstream source(file);
+    string textline;
+    while(getline(source,textline)){
+        stringstream s(textline);
+        string temp;
+        MenuItem m;
 
+        //นำเข้าข้อมูลเลขเมนู
+        getline(s, temp, ',');
+        m.id = stoi(temp);
+        
+        //นำเข้าข้อมูลชื่อเมนู
+        getline(s,m.name, ',');
+        
+        //นำเข้าข้อมูลราคา
+        getline(s, temp, ',');
+        m.price = stod(temp);
+        
+        //นำเข้าข้อมูลขายอยู่ไหม
+        getline(s,temp,',');
+        if(temp == "true") m.available = true;
+        else m.available = false;
+        
+        //นำเข้าข้อมูลคำอธิบาย
+        getline(s,m.descripstion,',');
+
+        menu.push_back(m);
+    }
+    source.close();
 }
 
 //ฟังก์ชันแสดงเมนุสำหรับทดสอบ
 void CartSystem::ShowMenuForTest(){
-
+    cout << "------------------------------ MENU ------------------------------\n";
+    //ส่วนหัวคอลัมป์
+        cout << left << setw(4) << "ID";
+        cout << "| " << left << setw(15) << "Name";
+        cout << "| " << left << setw(8) << "Price";
+        cout << "| " << left << setw(12) << "Stat";
+        cout << "| " << left << setw(20) << "Descripstion" << endl;
+        cout << "------------------------------------------------------------------\n";
+    for(int i = 0; i < menu.size(); i++){
+        //ข้อมูลข้างใน
+        cout << left << setw(4) << menu[i].id;
+        cout << "| " << left << setw(15) << menu[i].name; 
+        cout << "| " << left << setw(8) << fixed << setprecision(2) << menu[i].price;
+        if(menu[i].available) cout << "| " << left << setw(12) << "Available"; //เช็คว่ามีอยู่มั้ย
+        else cout << "| " << left << setw(12) << "Sold out";
+        cout << "| " << left << setw(20) << menu[i].descripstion << endl;
+    }
+    cout << "------------------------------------------------------------------\n";
 }
 
 //ฟังก์ชันเพิ่มลงรถเข็น
@@ -55,13 +104,35 @@ void CartSystem::addToCart(int id){
             for(int i = 0; i < cart.size(); i++){ //เช็คว่ามีเมนูนี้ในรถเข็นอยู่แล้วรึป่าว
                 if(cart[i].item.id == id){
                     cart[i].quantity++;
-                    cout << "Menu + 1\n";
+                    cout << menu[i].name << " + 1\n";
                     return;
                 }
             }
             cart.push_back({menu[i],1}); //เพิ่มเมนูนั้นลงรถเข็น
+            cout << "Add " << menu[i].name << " to cart.\n";
             return;
         }
-        cout << "Menu not found.\n";
     }
+    cout << "Menu not found.\n";
+}
+
+//ฟังก์ชันลบออกจากรถเข็น
+void CartSystem::removeFromCart(int id){
+    cout << "Not yet\n";
+}
+
+//ฟังก์ชันคำนวณราคา
+double CartSystem::calculate(){
+    int sum = 0;
+    return sum;
+}
+
+//ฟังก์ชันแสดงของในรถเข็น
+void CartSystem::ShowCart(){
+    cout << "----------\n";
+}
+
+//ฟังก์ชันแสดงใบจอง
+void CartSystem::ShowReceipt(){
+    cout << "HAHA\n";
 }
