60d11d4334f09b65daf7a9f4ae2e66b423a64f93
ming <mingsa405@gmail.com>
2026-03-04 22:53:22 +0700

    up

---
+++ src/account.h
@@ -0,0 +1,9 @@
+#ifndef ACCOUNTWINDOW_H
+#define ACCOUNTWINDOW_H
+
+#include <windows.h>
+
+void CreateAdminWindow(HINSTANCE hInstance);
+void CreateUserWindow(HINSTANCE hInstance);
+
+#endif
---
+++ src/menu.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+#include <string>
+
+// ===== struct =====
+struct MenuItem {
+    int id;
+    std::wstring name;
+    double price;
+};
+
+// ===== global variables =====
+extern std::vector<MenuItem> menu;
+extern int scrollOffset;
+extern bool showCartPage;
+extern std::vector<HWND> addButtons;
+
+// ===== constants =====
+#define BTN_CART 500
+#define BTN_ADD_BASE 1000
+
+// ===== function prototypes =====
+void LoadMenuData();
+
+int GetMaxScroll(int clientH, int itemCount);
+void UpdateScrollBar(HWND hwnd, int contentItems);
+
+void DrawMenu(HDC hdc, RECT rc);
+void DrawCart(HDC hdc);
+void ShowAddButtons(bool show);
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
+                         WPARAM wParam, LPARAM lParam);
+HWND CreateMenuWindow(HINSTANCE hInstance);
